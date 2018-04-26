139fd8c163a48868f0edefabc74683162e4dfd15
liuweiwei <liuweiwei@domob.cn>
2018-04-26 12:42:05 +0800

    add remove

--- vue-test/src/utils/watcher.js
+++ vue-test/src/utils/watcher.js
@@ -16,6 +16,21 @@ Event.prototype = {
     fns.map(fn => {
       fn(args)
     })
+  },
+  remove: (keys, fn) => {
+    const fns = this.clientList[keys]
+    if (!fns) {
+      return
+    }
+    if (!fn) {
+      fns && (fns.length = 0)
+    } else {
+      fns.map((cur, index) => {
+        if (cur === fn) {
+          fns.splice(index, 1)
+        }
+      })
+    }
   }
 }
 var ee = new Event()
@@ -25,5 +40,6 @@ ee.listen('es', size => {
 ee.listen('ew', count => {
   console.log('数量是' + count)
 })
+ee.remove('es')
 ee.trigger('es', 20)
 ee.trigger('ew', 10)
